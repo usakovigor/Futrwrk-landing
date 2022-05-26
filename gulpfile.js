9bbed02d9d4b7e41a424dd619aea3e5a681e1b47
petyastoleru <petyastoleru00@gmail.com>
2022-05-26 23:15:09 +0300

    create landing page

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+const PATHS = {
+    build: {
+        html: `dist/`,
+        css: `dist/`,
+        js: `dist/js/`,
+        assets: `dist/assets/`,
+    },
+    src: {
+        html: 'src/*.html',
+        css: 'src/scss/styles.scss',
+        js: 'src/js/*.js',
+        assets: `src/assets/**`
+    },
+    watch: {
+        html: 'src/**/*.html',
+        css: 'src/scss/**/*.scss',
+        js: 'src/js/**/*.js',
+        assets: `src/assets/**`
+    },
+    clean: './dist/'
+}
+
+const {src, dest} = require('gulp');
+const gulp = require('gulp');
+const del = require('del');
+const csso = require('gulp-csso');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const jsMinify = require('gulp-minify');
+
+
+let browsersync = require('browser-sync').create();
+
+function browserSync() {
+    browsersync.init({
+        server: {
+            baseDir: './dist'
+        }
+    })
+}
+
+function html() {
+    return src(PATHS.src.html)
+        .pipe(dest(PATHS.build.html))
+        .pipe(browsersync.stream())
+}
+
+function assets() {
+    return src(PATHS.src.assets)
+        .pipe(dest(PATHS.build.assets))
+        .pipe(browsersync.stream())
+}
+
+function css() {
+    return src(PATHS.src.css)
+        .pipe(sass())
+        .pipe(autoprefixer(['last 15 versions', '> 1%'], {cascade: false}))
+        .pipe(csso({
+            restructure: false,
+            sourceMap: true,
+            debug: true
+        }))
+        .pipe(dest(PATHS.build.css))
+        .pipe(browsersync.stream())
+}
+
+function js() {
+    return src(PATHS.src.js, {allowEmpty: true})
+        .pipe(jsMinify({
+            noSource: true, ext: {
+                min: '.js'
+            },
+        }))
+        .pipe(dest(PATHS.build.js))
+        .pipe(browsersync.stream())
+}
+
+function watchFiles() {
+    gulp.watch([PATHS.watch.html], html)
+    gulp.watch([PATHS.watch.css], css)
+    gulp.watch([PATHS.watch.js], js)
+    gulp.watch([PATHS.watch.assets], assets)
+}
+
+function clean() {
+    return del(PATHS.clean)
+}
+
+const build = gulp.series(clean, gulp.parallel(css, html, js, assets))
+const watch = gulp.parallel(watchFiles, build, browserSync);
+
+exports.css = css;
+exports.html = html;
+exports.assets = assets;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
---
+++ src/js/script.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const domSelect = document.getElementById("a-select");
+const form = document.getElementById('form');
+// Initialize custom select;
+const select = NiceSelect.bind(domSelect);
+// initialize form validator
+const pristine = new Pristine(form);
+
+
+domSelect.addEventListener("change", () => {
+    this.validateSelect();
+});
+
+select.dropdown.addEventListener('focusout', () => {
+    this.validateSelect();
+})
+
+form.addEventListener('submit', e => onFormSubmit(e));
+
+async function onFormSubmit(event) {
+    event.preventDefault();
+    const selectValid = validateSelect();
+
+    if (pristine.validate() && selectValid) {
+        const dataObj = {
+            first_name: document.getElementById('name').value,
+            last_name: document.getElementById('lastname').value,
+            email: document.getElementById('email').value,
+            user_type: document.getElementById('a-select').value
+        };
+
+        if (Object.values(dataObj).some((val) => !!!val)) {
+            return;
+        }
+
+        const ENDPOINT_URL = 'http://earlysignup.us-east-1.elasticbeanstalk.com/api/early-adopter/';
+        fetch(ENDPOINT_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataObj),
+        }).then((res) => {
+            if (res.ok) {
+                clearForm();
+                successModalAction(true);
+            } else {
+                alert(`HTTP Error: ${res.statusText}`);
+            }
+        })
+
+    }
+}
+
+function clearForm() {
+    form.reset();
+    select.clear();
+}
+
+function validateSelect() {
+    const formSelect = document.getElementById('form-select');
+    const error = document.getElementById('form-select-error');
+
+    if (select.options.some(option => option.element.classList.contains('selected'))) {
+        formSelect.classList.remove('has-danger-custom');
+        error.innerText = '';
+        return true;
+    } else {
+        formSelect.classList.add('has-danger-custom');
+        error.innerText = 'Please choose.';
+        return false;
+    }
+}
+
+function successModalAction(show) {
+    const modalElement = document.querySelector('.success-modal');
+    modalElement.style.display = show ? 'flex' : 'none';
+    const hideModalListenerCallbackFn = () => successModalAction(false);
+    if (show) {
+        modalElement.addEventListener('click', hideModalListenerCallbackFn)
+    } else {
+        modalElement.removeEventListener('click', hideModalListenerCallbackFn);
+    }
+}
+
