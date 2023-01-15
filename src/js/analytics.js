b0accd0b6f96f12d21cff43a76851eec15a60234
petyastoleru <petyastoleru00@gmail.com>
2023-01-15 13:47:44 +0100

    add facebook analytic script

---
+++ src/js/analytics.js
@@ -0,0 +1,17 @@
+<!-- Google tag (gtag.js) - Google Analytics -->
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-253705346-1');
+
+<!-- Meta Pixel Code -->
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '665153995356797');
+fbq('track', 'PageView');
