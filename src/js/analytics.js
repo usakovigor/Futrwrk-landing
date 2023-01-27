5bfb202b9b5bc256d0f4fbce775e3fdad837c0a6
petyastoleru <petyastoleru00@gmail.com>
2023-01-27 19:52:06 +0100

    add one more meta script and domain verification meta

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -15,3 +15,14 @@ gtag('config', 'UA-253705346-1');
     'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '891535268556066');
 fbq('track', 'PageView');
+
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1539988903142086');
+fbq('track', 'PageView');
